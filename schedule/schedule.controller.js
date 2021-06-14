00c35b10e78e5886c8a447f66b13ab1513221f6d
zaryanz <aryanx404@gmail.com>
2021-06-14 13:15:36 +0530

    added date validation

--- schedule/schedule.controller.js
+++ schedule/schedule.controller.js
@@ -2,17 +2,23 @@ const Model = require("./schedule.model");
 const moment = require("moment");
 
 exports.getSchedule = (req, res) => {
-  const { date } = req.params;
-  Model.getSchedule(
-    {
-      date,
-    },
-    (err, data) => {
-      if (err) {
-        return res.status(500).json({ error: "Server error" });
-      } else {
-        return res.json({ data });
+  const dateInput = req.params.date;
+  let date = moment(dateInput, "YYYY-MM-DD", true);
+  if (date.isValid()) {
+    date = date.format("YYYY-MM-DD");
+    Model.getSchedule(
+      {
+        date,
+      },
+      (err, data) => {
+        if (err) {
+          return res.status(500).json({ error: "Server error" });
+        } else {
+          return res.json({ data });
+        }
       }
-    }
-  );
+    );
+  } else {
+    return res.status(422).json({ error: "Invalid date" });
+  }
 };
